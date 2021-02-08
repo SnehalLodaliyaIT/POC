c284eede2fc7d12c0eea3be74f79de282b548775
SnehalLodaliya <snehal.lodaliya@coruscate.work>
2021-02-08 09:05:01 +0530

    demo of curl using node

---
+++ POCApplication/DemoOfCurlUsingCode.js
@@ -0,0 +1,34 @@
+var express = require('express');
+const axios = require('axios')
+const stripe = require('stripe')('sk_test_51IGOMfBuOumXPWbaDEIJsTcnOJY2QdFc1CPiqLLjLmypWdoRzIFrCvPBduVUXeOhTELNxyM2V36s0xkdlh4v2t9E005eCsYGcr');
+var router = express.Router();
+var exec = require('child_process').exec;
+
+router.get('/getCustomerCheckCurl', async function (req, res, next) {
+    var args = "  https://api.stripe.com/v1/customers  -u 'sk_test_51IGOMfBuOumXPWbaDEIJsTcnOJY2QdFc1CPiqLLjLmypWdoRzIFrCvPBduVUXeOhTELNxyM2V36s0xkdlh4v2t9E005eCsYGcr :'";
+
+    const customer=exec('curl ' + args, function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        res.send(stdout);
+    });
+    
+});
+
+
+
+module.exports = router;
+
+
+
+
+//Example of curl for run in terminal
+// curl https://api.stripe.com/v1/customers \
+//   -u curl https://api.stripe.com/v1/customers \
+//   -u sk_test_51IGOMfBuOumXPWbaDEIJsTcnOJY2QdFc1CPiqLLjLmypWdoRzIFrCvPBduVUXeOhTELNxyM2V36s0xkdlh4v2t9E005eCsYGcr: \
+//   -d limit=3 \
+//   -d limit=3 \
+//   -G
