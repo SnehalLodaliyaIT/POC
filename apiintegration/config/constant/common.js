8a7ea90483700e4603803c08a9e5887970f6b704
Snehal.Lodaliya <snehal.lodaliya@coruscate.work>
2021-02-17 19:26:23 +0530

    Done with create and delete for add api in file

---
+++ apiintegration/config/constant/common.js
@@ -0,0 +1,16 @@
+module.exports = {
+    LANGUAGE_CODE: {
+        cURL: 1,
+        JavaScript: 2,
+        jQuery: 3,
+        Node: 4,
+        Python: 5,
+        PHP: 6
+    },
+    PARAMETER_TYPE:{
+        REQUEST_PARAMETER:1,
+        PATH_PARAMETER:2,
+        QUERY_PARAMETER:3,
+        
+    }
+}
