a722262d547cc8114b0524079dec08aae7d11898
saloni.saraiya <saloni.saraiya@coruscate.work>
2021-03-17 13:18:44 +0530

    one signal module added

---
+++ apiintegration/CCAvenue/constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "merchant_id": "",
+    "access_code": "",
+    "working_key": ""
+}
---
+++ apiintegration/OneSignal/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+    app_id: "14b7494e-1c0f-472c-a3f9-0fb57d246b70",
+    api_key: "NmVkZTZhODMtYmMzZi00YTllLThmZmMtZWMxNGEwZWY3ZDE3"
+}
